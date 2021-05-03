9f1399f0528ca3f3774b68964e45a2252a1ab228
Vinzenz Branzk <vinzenz.branzk@mni.thm.de>
2021-05-03 07:03:05 +0200

    Add simple navigation bloc

--- lama_app/lib/main.dart
+++ lama_app/lib/main.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 void main() {}
+
+enum NavigationEvent { home, task, snake }
+
+@immutable
+abstract class NavigationState {}
+
+class NavigationHomeState extends NavigationState {}
+class NavigationSnakeState extends NavigationState {}
+class NavigationTaskState extends NavigationState {}
+
+
+class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
+  NavigationBloc() : super(NavigationHomeState());
+
+  @override
+  Stream<NavigationState> mapEventToState(NavigationEvent event) async* {
+    switch (event) {
+      case NavigationEvent.home : {
+        yield NavigationHomeState();
+        break;
+      }
+      case NavigationEvent.task : {
+        yield NavigationTaskState();
+        break;
+      }
+      case NavigationEvent.snake : {
+        yield NavigationSnakeState();
+        break;
+      }
+    }
+  }
+}
