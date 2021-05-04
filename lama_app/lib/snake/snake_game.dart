046ca8127e5f62f423ac361a87b84f09f8b67720
Vinzenz Branzk <vinzenz.branzk@mni.thm.de>
2021-05-04 06:10:35 +0200

    Add and render background

--- lama_app/lib/snake/snake_game.dart
+++ lama_app/lib/snake/snake_game.dart
@@ -1,14 +1,17 @@
 import 'dart:ui';
 
 import 'package:flame/gestures.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
+import 'package:lama_app/snake/components/background.dart';
 
 class SnakeGame extends Game with TapDetector {
   Size screenSize;
   double tileSize;
+  Background background;
 
   SnakeGame() {
     initialize();
@@ -16,9 +19,11 @@ class SnakeGame extends Game with TapDetector {
 
   void initialize() async {
     resize(await Flame.util.initialDimensions());
+    background = Background(this);
   }
 
   void render(Canvas canvas) {
+    background.render(canvas);
   }
 
   void update(double t) {
