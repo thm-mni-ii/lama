cb6c5de09c79290ba414549c6c04de06817dee87
Vinzenz Branzk <vinzenz.branzk@mni.thm.de>
2021-05-05 19:30:19 +0200

    Add snake movement

--- lama_app/lib/snake/components/snake.dart
+++ lama_app/lib/snake/components/snake.dart
@@ -1,28 +1,52 @@
 import 'dart:ui';
+import 'dart:developer' as developer;
 
 import 'package:lama_app/snake/models/position.dart';
 
 class Snake {
-  final Position headPos;
+  Position headPos;
   final double tileSize;
-  Rect bgRect;
-  Paint bgPaint;
+  final int fieldX;
+  final int fieldY;
 
-  Snake(this.headPos, this.tileSize) {
-    // rectangle
-    bgRect = Rect.fromLTWH(
-        (this.headPos.x - 1) * tileSize,
-        (this.headPos.y - 1) *  tileSize,
-        this.headPos.x + tileSize,
-        this.headPos.y + tileSize);
+  Snake(this.headPos, this.tileSize, this.fieldX, this.fieldY);
 
-    // paint
-    bgPaint = Paint();
-    bgPaint.color = Color(0xFF208421);
+  /// This method moves the snake by the given direction for 1 tile.
+  /// [dir] 1 = north, 2 = west, 3 = east everything else = south
+  void moveSnake(int dir) {
+    switch(dir) {
+      case 3 : {
+        headPos = headPos.x <= 1 ? Position(fieldX, headPos.y) : Position(headPos.x - 1, headPos.y);
+        break;
+      }
+      case 2 : {
+        headPos = headPos.x >= fieldX ? Position(1, headPos.y) : Position(headPos.x + 1, headPos.y);
+        break;
+      }
+      case 1 : {
+        headPos = headPos.y <= 1 ? Position(headPos.x, fieldY) : Position(headPos.x, headPos.y - 1);
+        break;
+      }
+      default : {
+        headPos = headPos.y >= fieldY ? Position(headPos.x, 1) : Position(headPos.x, headPos.y + 1);
+        break;
+      }
+    }
   }
 
   void render(Canvas c) {
-    c.drawArc(bgRect, 0, 360, true, bgPaint);
+    // rectangle
+    Rect bgRect = Rect.fromLTWH(
+        (this.headPos.x - 1) * tileSize,
+        (this.headPos.y - 1) * tileSize,
+        tileSize,
+        tileSize);
+
+    // paint
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xFF208421);
+
+    c.drawArc(bgRect, 0, 10, true, bgPaint);
   }
 
   void update(double t) {}
--- lama_app/lib/snake/snake_game.dart
+++ lama_app/lib/snake/snake_game.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 import 'dart:developer' as developer;
 
@@ -13,10 +14,20 @@ import 'components/snake.dart';
 import 'models/position.dart';
 
 class SnakeGame extends Game with TapDetector {
-  Size screenSize;
-  double tileSize;
+  final bool debugMovement = true;
+
   Background background;
   Snake snake;
+  double snakeVelocity = 5;
+  int snakeDirection = 1;
+
+  double deltaCounter = 0;
+  Random rnd = Random();
+
+  Size screenSize;
+  double tileSize;
+  final maxFieldX = 20;
+  final maxFieldY = 20;
 
   SnakeGame() {
     initialize();
@@ -24,8 +35,9 @@ class SnakeGame extends Game with TapDetector {
 
   void initialize() async {
     resize(await Flame.util.initialDimensions());
+
     background = Background(this);
-    snake = Snake(Position(5,5), tileSize);
+    snake = Snake(Position(1, 1), tileSize, 20, 20);
   }
 
   void render(Canvas canvas) {
@@ -34,7 +46,14 @@ class SnakeGame extends Game with TapDetector {
   }
 
   void update(double t) {
-
+    deltaCounter += t;
+    // moves the snake depending on its velocity
+    if (deltaCounter / (1 / snakeVelocity) > 1.0) {
+      // when debug_movement is active the snake moves towards an random direction
+      snake.moveSnake(debugMovement ? rnd.nextInt(3) : snakeDirection);
+      // resets the deltaCounter
+      deltaCounter = 0;
+    }
   }
 
   void onTapDown(TapDownDetails d) {
@@ -42,7 +61,7 @@ class SnakeGame extends Game with TapDetector {
 
   void resize(Size size) {
     screenSize = size;
-    tileSize = screenSize.width / 20;
+    tileSize = screenSize.width / maxFieldX;
 
     developer.log("The screensize is $screenSize");
     developer.log("The tilesize is $tileSize");
