ff39acf69ba325320b0d4ed3ee483386cf64659b
Kevin Jonathan Binder <kevin.jonathan.binder@mni.thm.de>
2021-05-05 15:43:46 +0200

    Add Header and modify SafeArea

--- lama_app/lib/app/screens/home_screen.dart
+++ lama_app/lib/app/screens/home_screen.dart
@@ -4,22 +4,39 @@ import 'package:lama_app/app/screens/game_screen.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.red,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(child: Text("Mathe"), onPressed: () => {}),
-              ElevatedButton(
-                child: Text("Snake"),
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GameScreen()),
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.white,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(child: Text("Mathe"), onPressed: () => {}),
+                    ElevatedButton(
+                      child: Text("Snake"),
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => GameScreen()),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: screenSize.width,
+                height: 50,
+                color: Colors.red,
+              ),
+            ),
+          ],
         ),
       ),
     );
