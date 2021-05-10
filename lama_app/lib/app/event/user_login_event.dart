c4f02d91a6b604ca6c8ea2a041fed3bd9a556f56
lkmm73 <lars.kammerer@mni.thm.de>
2021-05-10 15:38:08 +0200

    Change and create new States and Events
    Default State is now Usersloaded
    UserLoginFailed holds an error message now
    
    add event loadUsers

--- lama_app/lib/app/event/user_login_event.dart
+++ lama_app/lib/app/event/user_login_event.dart
@@ -2,10 +2,17 @@ import 'package:lama_app/app/user.dart';
 
 abstract class UserLoginEvent {}
 
+class LoadUsers extends UserLoginEvent {}
+
 class UserLogin extends UserLoginEvent {
   User user;
   String pw;
   UserLogin(this.user, this.pw);
+}
+
+class SelectUser extends UserLoginEvent {
+  User user;
+  SelectUser(this.user);
 }
 
 class UserLoginAbort extends UserLoginEvent {}
--- lama_app/lib/app/state/user_login_state.dart
+++ lama_app/lib/app/state/user_login_state.dart
@@ -2,9 +2,19 @@ import 'package:lama_app/app/user.dart';
 
 abstract class UserLoginState {}
 
+class UsersLoaded extends UserLoginState {
+  List<User> userList;
+  UsersLoaded(this.userList);
+}
+
 class UserSelected extends UserLoginState {
   User user;
   UserSelected(this.user);
+}
+
+class UserLoginFailed extends UserLoginState {
+  String error;
+  UserLoginFailed({this.error = 'Da ist wohl etwas gehörig schief gelaufen'});
 }
 
 class UserLoginSuccessful extends UserLoginState {}
