e6078e9f1da2bf377a1768603e1cc59505510bae
Kevin Jonathan Binder <kevin.jonathan.binder@mni.thm.de>
2021-05-10 12:54:27 +0200

    Added the TasksetRepository class

--- lama_app/lib/app/bloc/choose_taskset_bloc.dart
+++ lama_app/lib/app/bloc/choose_taskset_bloc.dart
@@ -1,13 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lama_app/app/event/choose_taskset_events.dart';
+import 'package:lama_app/app/repository/taskset_repository.dart';
 import 'package:lama_app/app/state/choose_taskset_state.dart';
+import 'package:lama_app/app/task-system/taskset_model.dart';
 
 class ChooseTasksetBloc extends Bloc<ChooseTasksetEvent, ChooseTasksetState> {
-  ChooseTasksetBloc() : super(LoadingAllTasksetsState());
+  TasksetRepository repository;
+
+  ChooseTasksetBloc(this.repository) : super(LoadingAllTasksetsState());
 
   @override
   Stream<ChooseTasksetState> mapEventToState(ChooseTasksetEvent event) async* {
     if (event is LoadAllTasksetsEvent) yield LoadingAllTasksetsState();
+    List<Taskset> tasksets =
+        repository.getTasksetsForSubjectAndGrade("Mathe", 2);
+    for (Taskset t in tasksets) print(t.name);
     //TODO fetch all tasksets from repository
     //TODO return a state which contains all tasksets
   }
