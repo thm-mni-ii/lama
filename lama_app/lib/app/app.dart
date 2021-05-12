1d8294bea04d48790e0dcf760b7cf7bb7f042587
lkmm73 <lars.kammerer@mni.thm.de>
2021-05-12 18:05:39 +0200

    Change default Screen to UserSelection in app.dart
    The App starts with User Selecton
    To move to the Homescreen the default user admin (passwort: "admin") is
    always true

--- lama_app/lib/app/app.dart
+++ lama_app/lib/app/app.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lama_app/app/screens/home_screen.dart';
+import 'package:lama_app/app/screens/user_login_screen.dart';
+
+import 'bloc/user_login_bloc.dart';
 
 class LamaApp extends MaterialApp {
-  LamaApp() : super(home: HomeScreen());
+  LamaApp()
+      : super(
+          home: BlocProvider(
+            create: (BuildContext context) => UserLoginBloc(),
+            child: UserLoginScreen(),
+          ),
+        );
 }
