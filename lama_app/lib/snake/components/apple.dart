b8364d4d5ba579efdab2b0d694636b64a9b87493
Vinzenz Branzk <vinzenz.branzk@mni.thm.de>
2021-05-12 05:35:21 +0200

    Fixed outer bound position bug

--- lama_app/lib/snake/components/apple.dart
+++ lama_app/lib/snake/components/apple.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'dart:ui';
+import 'dart:developer' as developer;
 
 import 'package:flutter/cupertino.dart';
 import 'package:lama_app/snake/snake_game.dart';
@@ -25,12 +26,12 @@ class Apple {
   /// [excludePositions] = this Positions will get ignored when finding a new one
   void setRandomPosition([List<Position> excludePositions]) {
     // new random Position
-    var newPosition = Position(_rnd.nextInt(this.game.maxFieldX + 1), _rnd.nextInt(this.game.maxFieldY) + this.game.fieldOffsetY + 1);
+    var newPosition = Position(_rnd.nextInt(this.game.maxFieldX) + 1, _rnd.nextInt(this.game.maxFieldY) + this.game.fieldOffsetY + 1);
 
     // exclude a list of Positions
     if (excludePositions != null) {
       while (excludePositions.any((pos) => pos.x == newPosition.x && pos.y == newPosition.y)) {
-        newPosition = Position(_rnd.nextInt(this.game.maxFieldX + 1), _rnd.nextInt(this.game.maxFieldY + 1));
+        newPosition = Position(_rnd.nextInt(this.game.maxFieldX) + 1, _rnd.nextInt(this.game.maxFieldY) + this.game.fieldOffsetY + 1);
       }
     }
 
@@ -42,6 +43,8 @@ class Apple {
         (newPosition.y - 1) * this.game.tileSize,
         this.game.tileSize,
         this.game.tileSize);
+
+    developer.log("[Apple][newRandomPosition] position = [x = ${newPosition.x}, y = ${newPosition.y}]");
   }
 
   void render(Canvas c) {
