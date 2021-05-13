b3d9ada053e701fc1e07d351f44d1b82839edca3
lkmm73 <lars.kammerer@mni.thm.de>
2021-05-13 16:49:03 +0200

    Issue: inserting a Admin results in crash
    inserting an Admin results in crash.
    
    Reason: Database needs the column 'Avatar'
    waiting for Fix

--- lama_app/lib/app/screens/create_admin_screen.dart
+++ lama_app/lib/app/screens/create_admin_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lama_app/app/bloc/user_login_bloc.dart';
+import 'package:lama_app/app/model/user_model.dart';
+import 'package:lama_app/app/screens/user_login_screen.dart';
+import 'package:lama_app/db/database_provider.dart';
 
 class CreateAdminScreen extends StatelessWidget {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final User _user = User(
+    name: '',
+    password: '',
+    grade: 0,
+    coins: 0,
+    isAdmin: true,
+    avatar: 'admin',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -11,37 +23,66 @@ class CreateAdminScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Nutzername'),
-            validator: (String value) {
-              return validatiorInput(value);
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Passwort'),
-            validator: (String value) {
-              return validatiorInput(value);
-            },
-          ),
+          SizedBox(height: 50),
           Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    //TODO Daten an Datenbank senden
-                  }
+              padding: EdgeInsets.all(20.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Nutzername', errorText: validatiorInputName()),
+                validator: (String value) {
+                  return validatiorInputPassword();
                 },
-                child: Text('Speichern')),
-          )
+                onChanged: (value) {
+                  _user.name = value;
+                  validatiorInputName();
+                },
+              )),
+          Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Passwort', errorText: validatiorInputPassword()),
+                validator: (String value) {
+                  return validatiorInputPassword();
+                },
+                onChanged: (value) => _user.password = value,
+              )),
+          SizedBox(height: 60),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(minimumSize: Size(250, 45)),
+              onPressed: () async {
+                if (validatiorInputName() == null &&
+                    validatiorInputPassword() == null) {
+                  await DatabaseProvider.db.insertUser(_user);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => BlocProvider(
+                                create: (BuildContext context) =>
+                                    UserLoginBloc(),
+                                child: UserLoginScreen(),
+                              )));
+                }
+              },
+              child: Text('Speichern')),
         ],
       ),
       backgroundColor: Color(0xFFFFFFFF),
     );
   }
 
-  String validatiorInput(String input) {
-    if (input == null || input.isEmpty) {
-      return 'Das Feld darf nicht leer sein!';
+  String validatiorInputName() {
+    if (_user.name == '' || _user.name.isEmpty || _user.name == null) {
+      return 'Der Nutzername darf nicht leer sein!';
+    }
+    return null;
+  }
+
+  String validatiorInputPassword() {
+    if (_user.password == '' ||
+        _user.password.isEmpty ||
+        _user.password == null) {
+      return 'Das Password darf nicht leer sein!';
     }
     return null;
   }
