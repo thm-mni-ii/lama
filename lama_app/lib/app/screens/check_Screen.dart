d5e13793243335d3cfed13f4223b603063f3cdb0
lkmm73 <lars.kammerer@mni.thm.de>
2021-05-14 16:50:31 +0200

    Add accept screen for DSGVO

--- lama_app/lib/app/screens/check_Screen.dart
+++ lama_app/lib/app/screens/check_Screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lama_app/app/bloc/check_screen_bloc.dart';
 import 'package:lama_app/app/event/check_screen_event.dart';
@@ -27,6 +28,38 @@ class CheckScreenPage extends State<CheckScreen> {
     return Scaffold(
       body: BlocBuilder<CheckScreenBloc, CheckScreenState>(
         builder: (context, state) {
+          if (state is ShowDSGVO) {
+            return Scaffold(
+              appBar: AppBar(title: Text('Datenschutzgrunderordnung')),
+              body: Column(
+                children: [
+                  Expanded(child: Markdown(data: state.dsgvo)),
+                  ElevatedButton(
+                    onPressed: () {
+                      context
+                          .read<CheckScreenBloc>()
+                          .add(DSGVOAccepted(context));
+                    },
+                    child: Text('Annehmen'),
+                    style: ElevatedButton.styleFrom(minimumSize: Size(400, 45)),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.read<CheckScreenBloc>().add(DSGVODenied());
+                    },
+                    child: Text('Ablehnen'),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(400, 45),
+                      primary: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
           return Container(
             alignment: Alignment.center,
             child: SvgPicture.asset(
--- lama_app/lib/app/state/check_screen_state.dart
+++ lama_app/lib/app/state/check_screen_state.dart
@@ -3,3 +3,10 @@ class CheckScreenState {}
 class AdminExist extends CheckScreenState {}
 
 class NoAdmin extends CheckScreenState {}
+
+class ShowDSGVO extends CheckScreenState {
+  String dsgvo;
+  ShowDSGVO(this.dsgvo);
+}
+
+class CreateAdmin extends CheckScreenState {}
