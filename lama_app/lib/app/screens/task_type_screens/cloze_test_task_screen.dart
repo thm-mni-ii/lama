8dce995c56003f1729a34b1c9895387e09c155fd
trnt54 <tobias.rentsch@mni.thm.de>
2021-05-18 22:01:57 +0200

    Setting up the Cloze Test task
    Creating cloze_test_task_screen,
    added the Cloze Task to task.dar,
    createtd simple taskset for cloze test task in deutsch3

---
+++ lama_app/lib/app/screens/task_type_screens/cloze_test_task_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ClozeTestTaskScreen extends StatelessWidget{
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container()
+    ]);
+  }
+}
