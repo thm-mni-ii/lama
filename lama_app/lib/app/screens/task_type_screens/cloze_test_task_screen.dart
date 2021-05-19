cb46492146f9e5fc484bb9a68bec038ac89dd9e2
trnt54 <tobias.rentsch@mni.thm.de>
2021-05-19 20:01:47 +0200

    Building and spacing the Answer boxes
    Creating the three boxes for the answeroptions in the Screen

--- lama_app/lib/app/screens/task_type_screens/cloze_test_task_screen.dart
+++ lama_app/lib/app/screens/task_type_screens/cloze_test_task_screen.dart
@@ -5,13 +5,13 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../task-system/task.dart';
 
-class ClozeTestTaskScreen extends StatelessWidget{
+class ClozeTestTaskScreen extends StatelessWidget {
   final TaskClozeTest task;
   final List<String> answers = [];
   final BoxConstraints constraints;
 
-  ClozeTestTaskScreen(this.task, this.constraints){
-    answers.add(task.rightAnswer);  //get the right answer
+  ClozeTestTaskScreen(this.task, this.constraints) {
+    answers.add(task.rightAnswer); //get the right answer
     answers.addAll(task.wrongAnswers); // add the wrong answers
     answers.shuffle(); // randomize in list
   }
@@ -20,51 +20,123 @@ class ClozeTestTaskScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Column(children: [
       Container(
-        height: (constraints.maxHeight / 100) * 30,
-        child: Align(
-          child: Text(
-            task.question,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
-            )
-          ),
-          alignment: Alignment.centerLeft,
-        )
-      ),
+          height: (constraints.maxHeight / 100) * 30,
+          child: Align(
+            child: Text(task.question,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                )),
+            alignment: Alignment.centerLeft,
+          )),
       Container(
         height: (constraints.maxHeight / 100) * 15,
-        padding: EdgeInsets.only(left: 15, right: 15), // create space between each childs
-        child: Stack(children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              padding: EdgeInsets.only(left: 75),
-              height: 50,
-              width: MediaQuery.of(context).size.width,
-              child: Bubble(
-                nip: BubbleNip.leftCenter,
-                child: Center(
-                  child: Text(
-                    task.lamaText,
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        padding: EdgeInsets.only(left: 15, right: 15),
+        // create space between each childs
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                padding: EdgeInsets.only(left: 75),
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: Bubble(
+                  nip: BubbleNip.leftCenter,
+                  child: Center(
+                    child: Text(
+                      task.lamaText,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: SvgPicture.asset(
+            Align(
+              alignment: Alignment.centerLeft,
+              child: SvgPicture.asset(
                 "assets/images/svg/lama_head.svg",
                 semanticsLabel: "Lama Anna",
-              width: 75,
-            ),
+                width: 75,
+              ),
+            )
+          ],
+        ),
+      ),
+      /* Container(
+        height: (constraints.maxHeight / 100) * 50,
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            height: 55,
+            width: 250,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.lightGreen,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: Offset(0, 3))
+                ]),
+          ),
+        ),
+      )*/
+      Container(
+        height: (constraints.maxHeight / 100) * 50,
+          alignment: Alignment.bottomCenter,
+          padding: EdgeInsets.only(top:20),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 55,
+            width: 250,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.lightGreen,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: Offset(0, 3))
+                ]),
+          ),
+          Container(
+            height: 55,
+            width: 250,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.lightGreen,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: Offset(0, 3))
+                ]),
+          ),
+          Container(
+            height: 55,
+            width: 250,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.lightGreen,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: Offset(0, 3))
+                ]),
           )
-        ],),
-      )
+        ],
+      ))
     ]);
   }
 }
