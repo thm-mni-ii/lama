7a9af94cd1fde3c8473d42d1fd7519a6004beec5
trnt54 <tobias.rentsch@mni.thm.de>
2021-05-23 23:17:56 +0200

    Added Lama Bubble and Done Button
    Added the Lama Speechbubble with the question in the top part of the
    task.
    Added the Done button into the bottom screen.

--- lama_app/lib/app/screens/task_type_screens/match_Category_task_screen.dart
+++ lama_app/lib/app/screens/task_type_screens/match_Category_task_screen.dart
@@ -1,5 +1,10 @@
+import 'package:bubble/bubble.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:lama_app/app/task-system/task.dart';
+import 'package:lama_app/util/LamaColors.dart';
+import 'package:lama_app/util/LamaTextTheme.dart';
 
 class MatchCategoryTaskScreen extends StatelessWidget{
   final TaskMatchCategory task;
@@ -14,8 +19,79 @@ class MatchCategoryTaskScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(children: [
+      // Lama Speechbubble
+      Container(
+        height: (constraints.maxHeight / 100) * 20,
+        padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+        // create space between each childs
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                padding: EdgeInsets.only(left: 75),
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: Bubble(
+                  nip: BubbleNip.leftCenter,
+                  child: Center(
+                    child: Text(
+                      task.lamaText,
+                      style:
+                      LamaTextTheme.getStyle(color: LamaColors.black, fontSize: 15),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: SvgPicture.asset(
+                "assets/images/svg/lama_head.svg",
+                semanticsLabel: "Lama Anna",
+                width: 75,
+              ),
+            )
+          ],
+        ),
+      ),
+
+      // Done Button
+      Container(
+        height: (constraints.maxHeight / 100) * 70,
+        alignment: Alignment.bottomCenter,
+        padding: EdgeInsets.only(top: 20),
+        //color: LamaColors.greenAccent,
+        child: Container(
+          height: 55,
+          width: 200,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: LamaColors.greenAccent,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: Offset(0, 3)),
+              ]),
+          child: InkWell(
+            child: Center(
+              child: Text(
+                "Fertig",
+                style: LamaTextTheme.getStyle(color: LamaColors.white, fontSize: 30,),
+              ),
+            ),
+          ),
+        ),
+      )
+
+
+
+
+
+    ],);
   }
   
 }
