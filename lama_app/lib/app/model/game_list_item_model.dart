35bd11eec3c85501916a7f939679dfa7403d37a0
Kevin Jonathan Binder <kevin.jonathan.binder@mni.thm.de>
2021-05-25 12:03:56 +0200

    Build the screen and add GameListItem model

---
+++ lama_app/lib/app/model/game_list_item_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameListItem {
+  String name;
+  int cost;
+  String desc;
+  Widget gameView;
+
+  GameListItem(this.name, this.cost, this.desc);
+}
--- lama_app/lib/app/screens/game_list_screen.dart
+++ lama_app/lib/app/screens/game_list_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lama_app/app/model/game_list_item_model.dart';
 import 'package:lama_app/util/LamaColors.dart';
+import 'package:lama_app/util/LamaTextTheme.dart';
 
 class GameListScreen extends StatelessWidget {
+  final List<GameListItem> games = [
+    GameListItem("Snake", 2,
+        "Steuer die Schlange mit den Pfeiltasten und sammle Früchte um länger zu werden!"),
+    GameListItem("Flappy-Lama", 3,
+        "Tippe auf den Bildschrim um das Lama fliegen zu lassen und weiche dabei den Hindernissen aus!"),
+    GameListItem("Woodcutter", 2,
+        "Tippe die entsprechende Richtung an um den Baum zu fällen und dabei den Ästen auszuweichen!"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +29,113 @@ class GameListScreen extends StatelessWidget {
             color: Colors.white,
             child: LayoutBuilder(
                 builder: (BuildContext context, BoxConstraints constraints) {
-              return Center(child: Text("GameList"));
+              return Column(children: [
+                Container(
+                  height: (constraints.maxHeight / 100) * 7.5,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [LamaColors.greenAccent, LamaColors.greenPrimary],
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(25),
+                      bottomRight: Radius.circular(25),
+                    ),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: IconButton(
+                          padding: EdgeInsets.all(0),
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 40,
+                            color: Colors.white,
+                          ),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                      ),
+                      Text(
+                        "Spiele",
+                        style: LamaTextTheme.getStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 20),
+                    child: LayoutBuilder(builder:
+                        (BuildContext context, BoxConstraints constraints) {
+                      return ListView.separated(
+                        itemCount: games.length,
+                        itemBuilder: (context, index) {
+                          return buildGameList(context, index, constraints);
+                        },
+                        separatorBuilder: (context, index) => SizedBox(
+                          height: (constraints.maxHeight / 100) * 5,
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ]);
             }),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildGameList(context, index, constraints) {
+    Color color = LamaColors.orangeAccent;
+    if (index % 2 == 0) color = LamaColors.blueAccent;
+    return Container(
+      height: (constraints.maxHeight / 100) * 25,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  games[index].name,
+                  style: LamaTextTheme.getStyle(fontSize: 30),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Text(
+                        games[index].cost.toString(),
+                        style: LamaTextTheme.getStyle(),
+                      ),
+                      SizedBox(width: 5),
+                      SvgPicture.asset(
+                        "assets/images/svg/lama_coin.svg",
+                        semanticsLabel: "Lama Coin",
+                        width: 25,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Text(
+              games[index].desc,
+              style: LamaTextTheme.getStyle(fontSize: 15),
+            )
+          ],
         ),
       ),
     );
