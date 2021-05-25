01460582ea51ca3cefdf1a52981df6e35225de11
lkmm73 <lars.kammerer@mni.thm.de>
2021-05-25 20:31:11 +0200

    Add excluding special chars from Userinput

--- lama_app/lib/util/input_validation.dart
+++ lama_app/lib/util/input_validation.dart
@@ -1,23 +1,23 @@
 abstract class InputValidation {
   static int allowedNameLength = 12;
-  static RegExp inputFilter = RegExp('[a-zA-Z0-9]');
+  static RegExp inputFilter = RegExp('[^a-zA-Z0-9]');
 
   static String inputUsernameValidation(String username) {
     if (isEmpty(username)) return 'Der Nutzername darf nicht leer sein!';
     if (username.length > allowedNameLength)
       return 'Der Nutzername darf nur $allowedNameLength Zeichen lang sein!';
-    //if (_regExpInvalide(username)) return 'Bitte keine Sonderzeichen!';
+    if (_regExpInvalide(username)) return 'Bitte keine Sonderzeichen!';
     return null;
   }
 
   static String inputPasswortValidation(String passwort) {
     if (isEmpty(passwort)) return 'Das Passwort darf nicht leer sein!';
-    //if (_regExpInvalide(passwort)) return 'Bitte keine Sonderzeichen!';
+    if (_regExpInvalide(passwort)) return 'Bitte keine Sonderzeichen!';
     return null;
   }
 
   static bool _regExpInvalide(String str) {
-    if (!inputFilter.hasMatch(str)) return true;
+    if (inputFilter.hasMatch(str)) return true;
     return false;
   }
 
