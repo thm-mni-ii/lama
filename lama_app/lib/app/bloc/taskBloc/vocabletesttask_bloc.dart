652cdd3332088e63e2eda62bd8f2bd432fc1a9a8
Kevin Jonathan Binder <kevin.jonathan.binder@mni.thm.de>
2021-06-07 22:34:48 +0200

    Started working on bloc

--- lama_app/lib/app/bloc/taskBloc/vocabletesttask_bloc.dart
+++ lama_app/lib/app/bloc/taskBloc/vocabletesttask_bloc.dart
@@ -1,21 +1,44 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lama_app/app/event/task_events.dart';
+import 'package:lama_app/app/task-system/task.dart';
 import 'package:lama_app/util/pair.dart';
 
 class VocableTestTaskBloc
     extends Bloc<VocableTestTaskEvent, VocableTestTaskState> {
-  List<Pair<String, String>> wordPairs;
+  final TaskVocableTest task;
+  final List<bool> resultList = [];
+  int curWordPair = 0;
 
-  VocableTestTaskBloc() : super(VocableTestTaskState());
+  VocableTestTaskBloc(this.task) : super(VocableTestTaskInitState());
 
   @override
-  Stream<VocableTestTaskState> mapEventToState(VocableTestTaskEvent event) {
-    // TODO: implement mapEventToState
-    throw UnimplementedError();
+  Stream<VocableTestTaskState> mapEventToState(
+      VocableTestTaskEvent event) async* {
+    if (event is VocableTestTaskGetWordEvent) {
+      yield VocableTestTaskTranslationState(
+          task.vocablePairs[curWordPair].a, resultList);
+    } else if (event is VocableTestTaskAnswerEvent) {
+      print(event.answer);
+    }
   }
 }
 
 class VocableTestTaskEvent {}
 
-class AnswerPairEvent extends VocableTestTaskEvent {}
+class VocableTestTaskGetWordEvent extends VocableTestTaskEvent {}
+
+class VocableTestTaskAnswerEvent extends VocableTestTaskEvent {
+  final String answer;
+  VocableTestTaskAnswerEvent(this.answer);
+}
 
 class VocableTestTaskState {}
+
+class VocableTestTaskInitState extends VocableTestTaskState {}
+
+class VocableTestTaskTranslationState extends VocableTestTaskState {
+  List<bool> resultList;
+  String wordToTranslate;
+
+  VocableTestTaskTranslationState(this.wordToTranslate, this.resultList);
+}
