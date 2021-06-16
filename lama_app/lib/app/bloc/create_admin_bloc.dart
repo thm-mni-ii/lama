602305b492b72dc5cb3247c5c095fed70a9a8429
lkmm73 <lars.kammerer@mni.thm.de>
2021-06-16 14:41:25 +0200

    Solve merge conflicts

--- lama_app/lib/app/bloc/create_admin_bloc.dart
+++ lama_app/lib/app/bloc/create_admin_bloc.dart
@@ -1,10 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lama_app/app/bloc/user_login_bloc.dart';
+import 'package:lama_app/app/bloc/user_selection_bloc.dart';
 import 'package:lama_app/app/event/create_admin_event.dart';
 import 'package:lama_app/app/model/user_model.dart';
-import 'package:lama_app/app/screens/user_login_screen.dart';
+import 'package:lama_app/app/screens/user_selection_screen.dart';
 import 'package:lama_app/app/state/create_admin_state.dart';
 import 'package:lama_app/db/database_provider.dart';
 
@@ -27,8 +27,8 @@ Future<void> _adminPush(User user, BuildContext context) async {
     context,
     MaterialPageRoute(
       builder: (context) => BlocProvider(
-        create: (BuildContext context) => UserLoginBloc(),
-        child: UserLoginScreen(),
+        create: (BuildContext context) => UserSelectionBloc(),
+        child: UserSelectionScreen(),
       ),
     ),
   );
