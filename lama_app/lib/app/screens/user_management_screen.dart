09c2c04eed69657b47e4f8bf7c1c2476b124cb31
lkmm73 <lars.kammerer@mni.thm.de>
2021-06-20 21:21:20 +0200

    Add SpeedDial for USerManagement Buttons

--- lama_app/lib/app/screens/user_management_screen.dart
+++ lama_app/lib/app/screens/user_management_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lama_app/app/bloc/user_management_bloc.dart';
 import 'package:lama_app/app/event/user_management_event.dart';
@@ -37,27 +38,7 @@ class UserManagementScreenState extends State<UserManagementScreen> {
           return Center(child: CircularProgressIndicator());
         },
       ),
-      floatingActionButton: Wrap(
-        children: [
-          FloatingActionButton(
-            heroTag: "addUser",
-            backgroundColor: LamaColors.mainPink,
-            onPressed: () =>
-                {context.read<UserManagementBloc>().add(CreateUser(context))},
-            tooltip: 'Nutzer hinzufügen',
-            child: Icon(Icons.group_add),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            heroTag: "addAdmin",
-            backgroundColor: LamaColors.bluePrimary,
-            onPressed: () =>
-                {context.read<UserManagementBloc>().add(CreateAdmin(context))},
-            tooltip: 'Admin hinzufügen',
-            child: Icon(Icons.group_add),
-          ),
-        ],
-      ),
+      floatingActionButton: _floatingButton(context),
     );
   }
 
@@ -116,5 +97,56 @@ class UserManagementScreenState extends State<UserManagementScreen> {
         );
       },
     );
+  }
+
+  Widget _floatingButton(BuildContext context) {
+    return SpeedDial(
+      icon: Icons.add,
+      backgroundColor: LamaColors.orangePrimary,
+      foregroundColor: LamaColors.white,
+      children: [
+        SpeedDialChild(
+          label: 'Nutzer hinzufügen',
+          backgroundColor: LamaColors.mainPink,
+          onTap: () =>
+              {context.read<UserManagementBloc>().add(CreateUser(context))},
+          child: Icon(
+            Icons.group_add,
+            color: LamaColors.white,
+          ),
+        ),
+        SpeedDialChild(
+          label: 'Administrator hinzufügen',
+          backgroundColor: LamaColors.bluePrimary,
+          onTap: () =>
+              {context.read<UserManagementBloc>().add(CreateAdmin(context))},
+          child: Icon(
+            Icons.group_add,
+            color: LamaColors.white,
+          ),
+        ),
+      ],
+    );
+    /*Wrap(
+      children: [
+        FloatingActionButton(
+          heroTag: "addUser",
+          backgroundColor: LamaColors.mainPink,
+          onPressed: () =>
+              {context.read<UserManagementBloc>().add(CreateUser(context))},
+          tooltip: 'Nutzer hinzufügen',
+          child: Icon(Icons.group_add),
+        ),
+        SizedBox(width: 10),
+        FloatingActionButton(
+          heroTag: "addAdmin",
+          backgroundColor: LamaColors.bluePrimary,
+          onPressed: () =>
+              {context.read<UserManagementBloc>().add(CreateAdmin(context))},
+          tooltip: 'Admin hinzufügen',
+          child: Icon(Icons.group_add),
+        ),
+      ],
+    );*/
   }
 }
