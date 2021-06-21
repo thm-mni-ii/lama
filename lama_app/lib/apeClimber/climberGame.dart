19ec8a21902a944288ee3358acf4f429c0bd898f
Vinzenz Branzk <vinzenz.branzk@mni.thm.de>
2021-06-21 09:19:51 +0200

    Rename fields

--- lama_app/lib/apeClimber/climberGame.dart
+++ lama_app/lib/apeClimber/climberGame.dart
@@ -1,9 +1,6 @@
-import 'package:flame/components/timer_component.dart';
 import 'package:flame/gestures.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
-import 'package:flame/time.dart';
-import 'dart:developer' as developer;
 
 import 'package:flutter/material.dart';
 import 'package:lama_app/apeClimber/components/MonkeyTimer.dart';
@@ -13,6 +10,8 @@ import 'package:lama_app/app/repository/user_repository.dart';
 import 'package:lama_app/app/model/highscore_model.dart';
 
 class ClimberGame extends BaseGame with TapDetector, HasWidgetsOverlay {
+  /// amount of tiles on the x coordinate
+  final int tilesX = 9;
   /// Timer component for display and organize the gametimer.
   MonkeyTimer _timer;
   /// name of the timer widget
@@ -20,23 +19,23 @@ class ClimberGame extends BaseGame with TapDetector, HasWidgetsOverlay {
   /// name of the start screen widget
   final startWidgetName = "start";
   /// a bool flag which indicates if the score of the game has been saved
-  bool _savedHighscore = false;
-  /// the personal highscore
+  bool _savedHighScore = false;
+  /// the personal highScore
   int _userHighScore;
-  /// the all time highscore in this game
-  int _alltimeHighScore;
+  /// the all time highScore in this game
+  int _allTimeHighScore;
+  /// the achieved score in this round
+  int score = 0;
+  /// necessary context for determine the actual screenSize
+  BuildContext _context;
 
   Size screenSize;
   double tileSize;
-  int tilesX = 9;
-  int tilesY;
-
-  int score = 0;
-
-  BuildContext _context;
+  /// amount of tiles on the y coordinate
+  int _tilesY;
 
   int _gameId = 3;
-
+  /// the [UserRepository] to interact with the database and get the user infos
   UserRepository _userRepo;
 
   ClimberGame(this._context, this._userRepo){
@@ -45,16 +44,16 @@ class ClimberGame extends BaseGame with TapDetector, HasWidgetsOverlay {
 
   void initialize() async {
     resize(await Flame.util.initialDimensions());
-    // load highscore
+    // load _serHighScore
     _userHighScore = await _userRepo.getMyHighscore(_gameId);
-    // load alltimeHighscore
-    _alltimeHighScore = await _userRepo.getHighscore(_gameId);
+    // load allTimeHighScore
+    _allTimeHighScore = await _userRepo.getHighscore(_gameId);
 
     addWidgetOverlay(
         startWidgetName,
         MonkeyStartWidget(
             userHighScore: _userHighScore,
-            alltimeHighScore: _alltimeHighScore,
+            alltimeHighScore: _allTimeHighScore,
             onStartPressed: _startGame)
     );
   }
@@ -79,10 +78,10 @@ class ClimberGame extends BaseGame with TapDetector, HasWidgetsOverlay {
   /// This method saves the actual Score to the database for the user which is logged in.
   ///
   /// sideffects:
-  ///   [_savedHighscore] = true
-  void _saveHighscore() {
-    if (!_savedHighscore) {
-      _savedHighscore = true;
+  ///   [_savedHighScore] = true
+  void _saveHighScore() {
+    if (!_savedHighScore) {
+      _savedHighScore = true;
       _userRepo.addHighscore(Highscore(
           gameID: _gameId,
           score: score,
@@ -112,11 +111,10 @@ class ClimberGame extends BaseGame with TapDetector, HasWidgetsOverlay {
         MediaQuery.of(_context).size.height - MediaQuery.of(_context).padding.top - MediaQuery.of(_context).padding.bottom);
 
     tileSize = screenSize.width / tilesX;
-    tilesY = screenSize.height ~/ tileSize;
+    _tilesY = screenSize.height ~/ tileSize;
 
     super.resize(size);
   }
 
-   void onTapDown(TapDownDetails d) {
-  }
+  void onTapDown(TapDownDetails d) { }
 }
