f82bed33bbf351682328e41268ec5dfd4c462e70
Vinzenz Branzk <vinzenz.branzk@mni.thm.de>
2021-06-21 06:55:26 +0200

    Add start screen widget

---
+++ lama_app/lib/apeClimber/widgets/monkeyStartWidget.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:lama_app/util/LamaColors.dart';
+
+/// This class is a [StatelessWidget] for displaying the start screen of Flappy lama
+class MonkeyStartWidget extends StatelessWidget {
+  final int userHighScore;
+  final int alltimeHighScore;
+  // This function will be called when start button is pressed.
+  final Function onStartPressed;
+
+  const MonkeyStartWidget({
+    this.userHighScore,
+    this.alltimeHighScore,
+    @required this.onStartPressed});
+
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Container (
+          height: MediaQuery.of(context).size.height * 0.85,
+          width: MediaQuery.of(context).size.width * 0.95,
+          child: Card(
+            margin: EdgeInsets.symmetric(
+                horizontal: 25.0,
+                vertical: 10.0
+            ),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(35.0)
+            ),
+            color: LamaColors.bluePrimary.withOpacity(0.8),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 15.0
+              ),
+              child: SafeArea(
+                child:  Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Flexible(
+                      child:Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/png/lama_head.png'
+                              ),
+                            )
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Flappy Lama",
+                      style: TextStyle(
+                        color: LamaColors.redPrimary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                      ),
+                    ),
+                    Text(
+                      "WIP",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: LamaColors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Flexible(
+                      child: Text("Mein Rekord: ${userHighScore.toString()}\n",
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            color: LamaColors.redAccent,
+                          )
+                      ),
+                    ),
+                    Flexible(
+                      child: Text("Rekord: ${alltimeHighScore.toString()}\n",
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            color: LamaColors.redAccent,
+                          )
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: LamaColors.redAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 35,
+                            vertical: 8
+                        ),
+                      ),
+                      child: Text(
+                        "Start",
+                        style: TextStyle(fontSize: 35.0),
+                      ),
+                      onPressed: () {
+                        onStartPressed.call();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+      ),
+    );
+  }
+}
