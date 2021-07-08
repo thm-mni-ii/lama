0df45b6c26b5ca3820e8b317d7fcf8dae35814de
lkmm73 <lars.kammerer@mni.thm.de>
2021-07-08 02:26:36 +0200

    delete unused package

--- lama_app/lib/app/bloc/admin_menu_bloc.dart
+++ lama_app/lib/app/bloc/admin_menu_bloc.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lama_app/app/event/admin_menu_event.dart';
 import 'package:lama_app/app/screens/admin_menu_screen.dart';
