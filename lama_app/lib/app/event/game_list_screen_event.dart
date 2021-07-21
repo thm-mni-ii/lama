34def10f67be660669c0177e1d76cef163853bc3
Kevin Jonathan Binder <kevin.jonathan.binder@mni.thm.de>
2021-07-21 12:30:16 +0200

    Add docs to my events

--- lama_app/lib/app/event/game_list_screen_event.dart
+++ lama_app/lib/app/event/game_list_screen_event.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
+/// BaseEvent for the [GameListBloc]
+///
+/// Author: K.Binder
 class GameListScreenEvent {}
 
+/// Subclass of [GameListScreenEvent]
+///
+/// This event contains the [gameCost], the [gameToStart] and the [context]
+/// of the that the [GameListBloc] will attempt to start.
+///
+/// Author: K.Binder
 class TryStartGameEvent extends GameListScreenEvent {
   int gameCost;
   String gameToStart;
