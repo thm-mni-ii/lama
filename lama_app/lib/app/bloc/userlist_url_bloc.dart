5205f74f8960a665b4f093e65e461f451f078748
lkmm73 <lars.kammerer@mni.thm.de>
2021-07-21 17:01:41 +0200

    add code doc UserlistUrlBloc

--- lama_app/lib/app/bloc/userlist_url_bloc.dart
+++ lama_app/lib/app/bloc/userlist_url_bloc.dart
@@ -9,10 +9,23 @@ import 'package:lama_app/app/state/userlist_url_state.dart';
 import 'package:lama_app/db/database_provider.dart';
 import 'package:lama_app/util/input_validation.dart';
 
+///[Bloc] for the [UserlistUrlScreen]
+///
+/// * see also
+///    [UserlistUrlScreen]
+///    [UserlistUrlEvent]
+///    [UserlistUrlState]
+///    [Bloc]
+///
+/// Author: L.Kammerer
+/// latest Changes: 15.06.2021
 class UserlistUrlBloc extends Bloc<UserlistUrlEvent, UserlistUrlState> {
   UserlistUrlBloc({UserlistUrlState initialState}) : super(initialState);
 
+  ///url that is used to parse the userlist
+  ///incoming events are used to change the value
   String _url;
+  //list of [User] parsed from the [_url]
   List<User> _userList = [];
 
   @override
@@ -29,6 +42,15 @@ class UserlistUrlBloc extends Bloc<UserlistUrlEvent, UserlistUrlState> {
     }
   }
 
+  ///(private)
+  ///parsing all users using [_parsUserList] from [_url] and
+  ///return them packed in [UserlistUrlParsingSuccessfull]
+  ///or return with an specific error message via [UserlistUrlParsingFailed] if an error occurred
+  ///validation of [_url] is handelt by [InputValidation.inputUrlWithJsonValidation]
+  ///
+  ///{@return}
+  ///[UserlistUrlParsingFailed] on error
+  ///[UserlistUrlParsingSuccessfull] via [_parsUserList] if the parsing was successfull
   Future<UserlistUrlState> _parsUrl() async {
     //Validat URL
     String error = await InputValidation.inputUrlWithJsonValidation(_url);
@@ -45,6 +67,20 @@ class UserlistUrlBloc extends Bloc<UserlistUrlEvent, UserlistUrlState> {
     }
   }
 
+  ///(private)
+  ///used to parse ever [User] providet by json
+  ///
+  ///the json should provide an key 'users' which is a list
+  ///with at least one [User] else an [UserlistUrlParsingFailed] with an suitable error message returns
+  ///also if an error from [User.isValidUser] is returned an [UserlistUrlParsingFailed] is returned by this function
+  ///if every [User] is successfully parsed an [List] with
+  ///all parsed [User] is packed in [UserlistUrlParsingSuccessfull] and returned
+  ///
+  ///{@param} Map<String, dynamic> as json
+  ///
+  ///{@return}
+  ///[UserlistUrlParsingFailed] with suitable error message if any error occurred
+  ///[UserlistUrlParsingSuccessfull] with an [List] of all parsed [User] if no error occurred
   UserlistUrlState _parsUserList(Map<String, dynamic> json) {
     //Check if UserList "users" exist in the json file
     if (!(json.containsKey('users') && json['users'] is List))
@@ -71,6 +107,8 @@ class UserlistUrlBloc extends Bloc<UserlistUrlEvent, UserlistUrlState> {
     return UserlistUrlParsingSuccessfull(_userList);
   }
 
+  ///(private)
+  ///stores every [User] given by [_userList] in to the database
   void _insertList() {
     _userList.forEach((user) async {
       await DatabaseProvider.db.insertUser(user);
