d4c7a6e92003df61083d7407bdc9787564b48043
lkmm73 <lars.kammerer@mni.thm.de>
2021-07-21 22:49:17 +0200

    add code doc all states

--- lama_app/lib/app/event/check_screen_event.dart
+++ lama_app/lib/app/event/check_screen_event.dart
@@ -6,19 +6,29 @@ import 'package:flutter/material.dart';
 /// latest Changes: 14.06.2021
 class CheckScreenEvent {}
 
+///used to triggers the check for admin process
+///
+///{@param}[BuildContext] as context for navigation
 class CheckForAdmin extends CheckScreenEvent {
   BuildContext context;
   CheckForAdmin(this.context);
 }
 
+///used to acknowledge the DSGVO
+///
+///{@param}[BuildContext] as context for navigation
 class DSGVOAccepted extends CheckScreenEvent {
   BuildContext context;
   DSGVOAccepted(this.context);
 }
 
+///used to navigate to the [CreateAdminScreen]
+///
+///{@param}[BuildContext] as context for navigation
 class CreateAdminEvent extends CheckScreenEvent {
   BuildContext context;
   CreateAdminEvent(this.context);
 }
 
+///used to denie the DSGVO
 class DSGVODenied extends CheckScreenEvent {}
--- lama_app/lib/app/state/edit_user_state.dart
+++ lama_app/lib/app/state/edit_user_state.dart
@@ -1,18 +1,38 @@
 import 'package:lama_app/app/model/user_model.dart';
 
+/// States used by [EditUserScreen] and [EditUserBloc]
+///
+/// Author: L.Kammerer
+/// latest Changes: 14.06.2021
 abstract class EditUserState {}
 
+///default state transmit user that should be edited
+///
+///{@param}[User] user that should be edited
 class EditUserDefault extends EditUserState {
   User user;
   EditUserDefault(this.user);
 }
 
+///acknowledge that all changes where succcesfull
+///
+///transmit [User] user with old values and [User] changedUser with
+///changed values
+///
+///{@params}
+///[User] user with old values
+///[User] changedUser with changed values
 class EditUserChangeSuccess extends EditUserState {
   User user;
   User changedUser;
   EditUserChangeSuccess(this.user, this.changedUser);
 }
 
+///used to double check the wish to delet an specific [User]
+///
+///tranmit an message that should be displayed (can contain warnings)
+///
+///{@param}[String] message (can contain warnings)
 class EditUserDeleteCheck extends EditUserState {
   String message;
   EditUserDeleteCheck(this.message);
