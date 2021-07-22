cef45b07dd9207db21cbc2bc187eecbf3dd18b4f
Kevin Jonathan Binder <kevin.jonathan.binder@mni.thm.de>
2021-07-22 10:22:11 +0200

    Add docs to all of my states

--- lama_app/lib/app/state/choose_taskset_state.dart
+++ lama_app/lib/app/state/choose_taskset_state.dart
@@ -1,9 +1,20 @@
 import 'package:lama_app/app/task-system/taskset_model.dart';
 
+///BaseState for the [ChooseTasksetBloc].
+///
+///Author: K.Binder
 abstract class ChooseTasksetState {}
 
+///Subclass of [ChooseTasksetState].
+///
+///Emitted while the [ChooseTasksetBloc] is loading all tasksets.
+///
+///Author: K.Binder
 class LoadingAllTasksetsState extends ChooseTasksetState {}
 
+///Subclass of [ChooseTasksetState].
+///
+///Emitted when all tasksets are loaded. They get passed via [tasksets].
 class LoadedAllTasksetsState extends ChooseTasksetState {
   List<Taskset> tasksets;
   LoadedAllTasksetsState(this.tasksets);
