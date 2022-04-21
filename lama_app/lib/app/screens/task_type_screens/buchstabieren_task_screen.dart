e6ed86120508f88d34bca02311c94a4fb57c5ba8
35Art37ur <33299597+35Art37ur@users.noreply.github.com>
2022-04-21 13:01:02 +0200

    Update buchstabieren_task_screen.dart

--- lama_app/lib/app/screens/task_type_screens/buchstabieren_task_screen.dart
+++ lama_app/lib/app/screens/task_type_screens/buchstabieren_task_screen.dart
@@ -21,7 +21,7 @@ import '../../task-system/task.dart';
 /// ClozeTestTaskScreen class creates the Cloze Test Task Screen
 
 class BuchstabierenTaskScreen extends StatefulWidget {
-  final Task task;
+  final TaskBuchstabieren task;
   final BoxConstraints constraints;
 
   BuchstabierenTaskScreen(this.task, this.constraints);
@@ -34,7 +34,7 @@ class BuchstabierenTaskScreen extends StatefulWidget {
 
 class BuchstabierenTaskState extends State<BuchstabierenTaskScreen> {
   // task infos and constraints handed over by tasktypeScreen
-  final Task task;
+  final TaskBuchstabieren task;
   final BoxConstraints constraints;
   // Value which is checked after pressing the "fertig" Button
   int i = 0;
@@ -42,59 +42,7 @@ class BuchstabierenTaskState extends State<BuchstabierenTaskScreen> {
   // var random = Random();
   //var rnd;
 
-  // ignore: non_constant_identifier_names
-  BuchstabierenTaskState(this.task, this.constraints) {
-    @override
-    Widget build(BuildContext context) {
-      return Column(children: [
-        // Lama Speechbubble
-        Container(
-          height: (constraints.maxHeight / 100) * 15,
-          padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-          // create space between each childs
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  padding: EdgeInsets.only(left: 75),
-                  height: 50,
-                  width: MediaQuery.of(context).size.width,
-                  child: Bubble(
-                    nip: BubbleNip.leftCenter,
-                    child: Center(
-                      child: Text(
-                        task.lamaText,
-                        style: LamaTextTheme.getStyle(
-                            color: LamaColors.black, fontSize: 15),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: SvgPicture.asset(
-                  "assets/images/svg/lama_head.svg",
-                  semanticsLabel: "Lama Anna",
-                  width: 75,
-                ),
-              ),
-            ],
-          ),
-        ),
-
-        Container(
-          decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-          height: MediaQuery.of(context).size.height / 5,
-          width: MediaQuery.of(context).size.width / 2,
-          child: SvgPicture.asset('assets/images/svg/Objects/Auto.svg'),
-        )
-      ]);
-    }
-  }
+  BuchstabierenTaskState(this.task, this.constraints);
 
   @override
   Widget build(BuildContext context) {
@@ -116,9 +64,9 @@ class BuchstabierenTaskState extends State<BuchstabierenTaskScreen> {
                   nip: BubbleNip.leftCenter,
                   child: Center(
                     child: Text(
-                      task.lamaText,
+                      task.woerter[0],
                       style: LamaTextTheme.getStyle(
-                          color: LamaColors.black, fontSize: 15),
+                          color: LamaColors.greenAccent, fontSize: 15),
                     ),
                   ),
                 ),
@@ -143,6 +91,38 @@ class BuchstabierenTaskState extends State<BuchstabierenTaskScreen> {
         height: MediaQuery.of(context).size.height / 5,
         width: MediaQuery.of(context).size.width / 2,
         child: SvgPicture.asset('assets/images/svg/Objects/Auto.svg'),
+      ),
+      Container(
+        margin: EdgeInsets.all(10),
+        height: 50.0,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("A", style: TextStyle(fontSize: 15)),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10),
+        height: 50.0,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("UUU", style: TextStyle(fontSize: 15)),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10),
+        height: 50.0,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("T", style: TextStyle(fontSize: 15)),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10),
+        height: 50.0,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("O", style: TextStyle(fontSize: 15)),
+        ),
       )
     ]);
   }
