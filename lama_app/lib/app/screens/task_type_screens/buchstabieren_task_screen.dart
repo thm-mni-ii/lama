9908aa03496d641145215cd56f71dbc199ce722c
JohannDch <93202113+JohannDch@users.noreply.github.com>
2022-04-21 11:24:18 +0200

    Update buchstabieren_task_screen.dart

--- lama_app/lib/app/screens/task_type_screens/buchstabieren_task_screen.dart
+++ lama_app/lib/app/screens/task_type_screens/buchstabieren_task_screen.dart
@@ -72,9 +72,10 @@ class BuchstabierenTaskScreen extends StatelessWidget {
         decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
         padding: EdgeInsets.all(10),
         margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-        height: MediaQuery.of(context).size.height /5,
-         width: MediaQuery.of(context).size.width /2,
-        child: SvgPicture.asset('assets/images/svg/Objects/Auto.svg'),)
-    ]); 
+        height: MediaQuery.of(context).size.height / 5,
+        width: MediaQuery.of(context).size.width / 2,
+        child: SvgPicture.asset('assets/images/svg/Objects/Auto.svg'),
+      )
+    ]);
   }
 }
