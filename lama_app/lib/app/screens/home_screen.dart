ec2ca987570eda659d9a7273f437939e61edea7f
InfaSg <stefan627@gmx.de>
2022-05-18 15:16:19 +0200

    now precaches images before task

--- lama_app/lib/app/screens/home_screen.dart
+++ lama_app/lib/app/screens/home_screen.dart
@@ -301,7 +301,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(50)))),
-          onPressed: () {
+          onPressed: () async {
             Navigator.push(
               context,
               MaterialPageRoute(
@@ -320,36 +320,7 @@ class _HomeScreenState extends State<HomeScreen> {
             //      die URL's der Bilder beinhaltet. die Variable Task wird jedoch erst in
             //      task_screen.dart gesetzt.
 
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Auto.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Baum.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Biene.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Faultier.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Fisch.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Frosch.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Hund.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Igel.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Jaguar.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Krokodil.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Küken.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Regenwurm.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Robbe.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Schmetterling.png?raw=true");
-            cacheImageByUrl2(context,
-                "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Schnecke.png?raw=true");
+            await preloadPngs();
           }));
       children.add(SizedBox(height: (constraints.maxHeight / 100) * 2.5));
     }
@@ -547,6 +518,49 @@ class _HomeScreenState extends State<HomeScreen> {
       return Future.value(false);
     } else {
       return Future.value(true);
+    }
+  }
+
+  ///This method gets called when clicking on the button "Deutsch" and preloads
+  ///all images for the "Buchstabieren" Task
+  ///
+  ///currently loads them everytime the button is pressed (I think)
+  Future<void> preloadPngs() async {
+    var pngs = {
+      "Auto":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Auto.png?raw=true",
+      "Baum":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Baum.png?raw=true",
+      "Biene":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Biene.png?raw=true",
+      "Faultier":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Faultier.png?raw=true",
+      "Fisch":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Fisch.png?raw=true",
+      "Frosch":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Frosch.png?raw=true",
+      "Hund":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Hund.png?raw=true",
+      "Igel":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Igel.png?raw=true",
+      "Jaguar":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Jaguar.png?raw=true",
+      "Krokodil":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Krokodil.png?raw=true",
+      "Küken":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Küken.png?raw=true",
+      "Regenwurm":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Regenwurm.png?raw=true",
+      "Robbe":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Robbe.png?raw=true",
+      "Schmetterling":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Schmetterling.png?raw=true",
+      "Schnecke":
+          "https://github.com/handitosb/lamaapps/blob/main/Bilder_Test/Schnecke.png?raw=true",
+    };
+    for (int i = 0; i < pngs.length; i++) {
+      await cacheImageByUrl2(context, pngs.values.toList()[i]);
+      print("I just loaded '${pngs.keys.toList()[i]}'");
     }
   }
 }
