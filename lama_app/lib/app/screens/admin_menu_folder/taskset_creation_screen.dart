1d9fd0cff870957e6c3ee0e0dfc05e32b649f769
nicosoethe <ni.soethe@gmail.com>
2022-06-08 19:23:37 +0200

    working on drowpdown

--- lama_app/lib/app/screens/admin_menu_folder/taskset_creation_screen.dart
+++ lama_app/lib/app/screens/admin_menu_folder/taskset_creation_screen.dart
@@ -20,7 +20,7 @@ import '../taskset_option_screen.dart';
 ///    [TasksetCreationEvent]
 ///    [TasksetCreationState]
 ///
-/// author: Nico Soethe
+/// author(s): Handito Bismo, Nico Soethe
 /// latest Changes: 08.06.2022
 class TasksetCreationScreen extends StatefulWidget {
   final BoxConstraints constraints;
@@ -38,6 +38,8 @@ class TasksetCreationScreenState extends State<TasksetCreationScreen> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   //temporary url to prevent losing the url on error states
   String urlInitValue;
+  var _currentSelectedValue;
+  var _currentSelectedValue2;
   final BoxConstraints constraints;
 
   TasksetCreationScreenState(this.constraints);
@@ -70,10 +72,9 @@ class TasksetCreationScreenState extends State<TasksetCreationScreen> {
       "Sachkunde"
     ];
     Size screenSize = MediaQuery.of(context).size;
-    var _currentSelectedValue;
-    var _currentSelectedValue2;
+
     return Scaffold(
-      appBar: CustomAppbar(size: screenSize.width/5, titel: "Taskset erstellen", color:  LamaColors.bluePrimary),
+      appBar: CustomAppbar(size: screenSize.width, titel: "Taskset erstellen", color:  LamaColors.bluePrimary),
       body: Stack(
         children: [
           Container(
@@ -83,29 +84,24 @@ class TasksetCreationScreenState extends State<TasksetCreationScreen> {
             Column(
               children: [
                 Container(
+                  margin: EdgeInsets.only(bottom: 10),
                   alignment: Alignment.centerLeft,
-                  child: Text.rich(
-                    TextSpan(
-                        text: 'Tasksetname',
-                        style: TextStyle(
-                          fontSize: 16,
-                        )),
-                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Tasksetname"
+                    ),
+                  )
                 ),
-                Divider(),
 
                 Container(
-                  margin: EdgeInsets.only(top: 15),
-                  alignment: Alignment.centerLeft,
-                  child: Text.rich(
-                    TextSpan(
-                        text: 'Kurzbeschreibung',
-                        style: TextStyle(
-                          fontSize: 16,
-                        )),
-                  ),
+                    margin: EdgeInsets.only(bottom: 10),
+                    alignment: Alignment.centerLeft,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Kurzbeschreibung"
+                      ),
+                    )
                 ),
-                Divider(),
 
                 Container(
                   margin: EdgeInsets.only(top: 45),
@@ -121,6 +117,7 @@ class TasksetCreationScreenState extends State<TasksetCreationScreen> {
                 ),
 
                 Container(
+                  margin: EdgeInsets.only(bottom: 10),
                   child: InputDecorator(
                       decoration: InputDecoration(
                         errorStyle: TextStyle(color: Colors.redAccent, fontSize: 16.0),
@@ -206,151 +203,8 @@ class TasksetCreationScreenState extends State<TasksetCreationScreen> {
               ),
             ),
           ),
-            ],
-          )
-        );
-    /*return Scaffold(
-      appBar: _bar(screenSize.width / 5, 'Tasksets erstellen', LamaColors.bluePrimary),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-            // create space between each childs
-            child: Stack(
-              children: [
-                 Column(
-                    children: [
-
-                      Container(
-                      alignment: Alignment.centerLeft,
-                      child: Text.rich(
-                        TextSpan(
-                            text: 'Tasksetname',
-                            style: TextStyle(
-                              fontSize: 16,
-                            )),
-                        ),
-                      ),
-                 Divider(),
-
-                      Container(
-                      margin: EdgeInsets.only(top: 15),
-                      alignment: Alignment.centerLeft,
-                      child: Text.rich(
-                        TextSpan(
-                            text: 'Kurzbeschreibung',
-                            style: TextStyle(
-                              fontSize: 16,
-                            )),
-                      ),
-                 ),
-                 Divider(),
-
-                      Container(
-                        margin: EdgeInsets.only(top: 45),
-                        alignment: Alignment.centerLeft,
-                        child: Text.rich(
-                        TextSpan(
-                            text: 'Klassenstufe',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            )),
-                      ),
-                      ),
-
-                      Container(
-                        child: InputDecorator(
-                          decoration: InputDecoration(
-                            errorStyle: TextStyle(color: Colors.redAccent, fontSize: 16.0),
-                            hintText: 'Klassenstufe auswählen',
-                            ), 
-                          isEmpty: _currentSelectedValue == '',
-                          child: DropdownButtonHideUnderline(  
-                            child: 
-                              DropdownButton<String>(
-                                value: _currentSelectedValue,
-                                isDense: true,
-                                onChanged: (String newValue){
-                                  setState(() {
-                                    _currentSelectedValue = newValue;
-                                  });
-                                },
-                                items: klassenStufe.map((String value){
-                                  return DropdownMenuItem<String>(
-                                    value: value,
-                                    child: Text(value)
-                              );
-                              }).toList(),
-                        ),)
-                          ),
-                        ),
-
-                        Container(
-                        margin: EdgeInsets.only(top: 15),
-                        alignment: Alignment.centerLeft,
-                        child: Text.rich(
-                        TextSpan(
-                            text: 'Fach',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            )),
-                      ),
-                      ),
-
-                        Container(
-                        child: InputDecorator(
-                          decoration: InputDecoration(
-                            errorStyle: TextStyle(color: Colors.redAccent, fontSize: 16.0),
-                            hintText: 'Fach auswählen',
-                            ), 
-                          isEmpty: _currentSelectedValue2 == '',
-                          child: DropdownButtonHideUnderline(  
-                            child: 
-                              DropdownButton<String>(
-                                value: _currentSelectedValue2,
-                                isDense: true,
-                                onChanged: (String newValue){
-                                  setState(() {
-                                    _currentSelectedValue2 = newValue;
-                                  });
-                                },
-                                items: facher.map((String value){
-                                  return DropdownMenuItem<String>(
-                                    value: value,
-                                    child: Text(value)
-                              );
-                              }).toList(),
-                        ),)
-                          ),
-                        ),
-
-                        Container(
-                        margin: EdgeInsets.only(top: 50),
-                        alignment: Alignment.bottomRight,
-                        child: ElevatedButton(
-                          onPressed: null,
-                          child: Text.rich(
-                                TextSpan(
-                                  text: 'Weiter',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                            )),
-                      ),
-                        )
-                        
-                      ),
-                 ],
-                )
-              ],
-            ),
-          ),
-          //Items
         ],
-      ),
-    );*/
+      )
+    );
   }
 }
--- lama_app/lib/app/screens/admin_menu_folder/widgets/custom_appbar.dart
+++ lama_app/lib/app/screens/admin_menu_folder/widgets/custom_appbar.dart
@@ -33,5 +33,5 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget{
 
   @override
   // TODO: implement preferredSize
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(size/5);
 }
