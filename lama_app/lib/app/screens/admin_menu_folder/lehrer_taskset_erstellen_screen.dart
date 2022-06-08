6a6fc31adf4b4b9411202328ef24816afd88a0ce
Handito Bismo <97729966+handitosb@users.noreply.github.com>
2022-06-08 19:56:19 +0200

    init

--- lama_app/lib/app/screens/admin_menu_folder/lehrer_taskset_erstellen_screen.dart
+++ lama_app/lib/app/screens/admin_menu_folder/lehrer_taskset_erstellen_screen.dart
@@ -16,8 +16,9 @@ import '../user_selection_screen.dart';
 //In this class, the teacher should be able to create a list of tasksets and save it to the server.
 //The teacher have the option to name the taskset in text field Tasksetname and the description in Kurzbeschreibung.
 class LehrerTasksetsErstellenScreen extends StatefulWidget {
-  String? value;
-  String? value2;
+  String? tasksetName;
+  String? kurzBeschreibung;
+
   @override
   LehrerTasksetsErstellenScreenState createState() =>
       LehrerTasksetsErstellenScreenState();
@@ -28,8 +29,13 @@ class LehrerTasksetsErstellenScreenState
     extends State<LehrerTasksetsErstellenScreen> {
   //[_formKey] should be used to identify every Form in this Screen
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? tasksetName;
+  String? kurzBeschreibung;
+  String? text;
+  String? text2;
   String? value;
   String? value2;
+
   LehrerTasksetsErstellenScreenState();
 
   @override
@@ -53,122 +59,149 @@ class LehrerTasksetsErstellenScreenState
     return Scaffold(
       appBar: navBar(screenSize.width / 5, "Tasksets erstellen",
           LamaColors.bluePrimary) as PreferredSizeWidget?,
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-            // create space between each childs
-            child: Stack(
-              children: [
-                Column(
-                  children: [
-                    Container(
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+              // create space between each childs
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                          alignment: Alignment.centerLeft,
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Tasksetname',
+                            ),
+                            validator: (text) {
+                              if (text == null || text.isEmpty) {
+                                return "Tasksetname fehlt!";
+                              }
+                              return null;
+                            },
+                            onSaved: (text) {
+                              tasksetName = text;
+                            },
+                            onChanged: (text) =>
+                                setState(() => this.tasksetName = text),
+                          )),
+                      Divider(),
+                      Container(
+                          margin: EdgeInsets.only(top: 15),
+                          alignment: Alignment.centerLeft,
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Kurzbeschreibung',
+                            ),
+                            validator: (text2) {
+                              if (text2 == null || text2.isEmpty) {
+                                return "Tasksetname fehlt!";
+                              }
+                            },
+                            onSaved: (text2) {
+                              kurzBeschreibung = text2;
+                            },
+                            onChanged: (text2) =>
+                                setState(() => this.kurzBeschreibung = text2),
+                          )),
+                      Divider(),
+                      Container(
+                        margin: EdgeInsets.only(top: 45),
                         alignment: Alignment.centerLeft,
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Tasksetname',
-                          ),
-                          onSaved: null,
-                          validator: null,
-                        )),
-                    Divider(),
-                    Container(
+                        child: Text.rich(
+                          TextSpan(
+                              text: 'Klassenstufe',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                      Container(
+                        child: InputDecorator(
+                            decoration: InputDecoration(
+                              errorStyle: TextStyle(
+                                  color: Colors.redAccent, fontSize: 16.0),
+                              hintText: 'Klassenstufe auswählen',
+                            ),
+                            isEmpty: value == '',
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<String>(
+                                value: value,
+                                items: klassenStufe.map(buildMenuItem).toList(),
+                                onChanged: (value) =>
+                                    setState(() => this.value = value),
+                              ),
+                            )),
+                      ),
+                      Container(
                         margin: EdgeInsets.only(top: 15),
                         alignment: Alignment.centerLeft,
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Kurzbeschreibung',
-                          ),
-                          onSaved: null,
-                          validator: null,
-                        )),
-                    Divider(),
-                    Container(
-                      margin: EdgeInsets.only(top: 45),
-                      alignment: Alignment.centerLeft,
-                      child: Text.rich(
-                        TextSpan(
-                            text: 'Klassenstufe',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                        child: Text.rich(
+                          TextSpan(
+                              text: 'Fach',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                      Container(
+                        child: InputDecorator(
+                            decoration: InputDecoration(
+                              errorStyle: TextStyle(
+                                  color: Colors.redAccent, fontSize: 16.0),
+                              hintText: 'Fach auswählen',
+                            ),
+                            isEmpty: value2 == '',
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<String>(
+                                value: value2,
+                                items: facher.map(buildMenuItem).toList(),
+                                onChanged: (value2) =>
+                                    setState(() => this.value2 = value2),
+                              ),
                             )),
                       ),
-                    ),
-                    Container(
-                      child: InputDecorator(
-                          decoration: InputDecoration(
-                            errorStyle: TextStyle(
-                                color: Colors.redAccent, fontSize: 16.0),
-                            hintText: 'Klassenstufe auswählen',
-                          ),
-                          isEmpty: value == '',
-                          child: DropdownButtonHideUnderline(
-                            child: DropdownButton<String>(
-                              value: value,
-                              items: klassenStufe.map(buildMenuItem).toList(),
-                              onChanged: (value) =>
-                                  setState(() => this.value = value),
+                      Container(
+                          margin: EdgeInsets.only(top: 10),
+                          alignment: Alignment.bottomRight,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Processing Data')),
+                                );
+                              }
+                              _formKey.currentState?.save();
+                              print(tasksetName);
+                              print(value);
+                              print(value2);
+                              print(kurzBeschreibung);
+                            },
+                            child: Text.rich(
+                              TextSpan(
+                                  text: 'Weiter',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  )),
                             ),
                           )),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 15),
-                      alignment: Alignment.centerLeft,
-                      child: Text.rich(
-                        TextSpan(
-                            text: 'Fach',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            )),
-                      ),
-                    ),
-                    Container(
-                      child: InputDecorator(
-                          decoration: InputDecoration(
-                            errorStyle: TextStyle(
-                                color: Colors.redAccent, fontSize: 16.0),
-                            hintText: 'Fach auswählen',
-                          ),
-                          isEmpty: value == '',
-                          child: DropdownButtonHideUnderline(
-                            child: DropdownButton<String>(
-                              value: value2,
-                              items: facher.map(buildMenuItem).toList(),
-                              onChanged: (value2) =>
-                                  setState(() => this.value2 = value2),
-                            ),
-                          )),
-                    ),
-                    Container(
-                        margin: EdgeInsets.only(top: 50),
-                        alignment: Alignment.bottomRight,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => TasksetsNameScreen()),
-                            );
-                          },
-                          child: Text.rich(
-                            TextSpan(
-                                text: 'Weiter',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                        )),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-          //Items
-        ],
+          ],
+        ),
+
+        //Items
       ),
     );
   }
