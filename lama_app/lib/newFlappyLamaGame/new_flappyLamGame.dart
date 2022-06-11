a204056416babb337fbb1f49e235b58d46708200
35Art37ur <33299597+35Art37ur@users.noreply.github.com>
2022-06-11 16:27:27 +0200

    smlChanges

--- lama_app/lib/newFlappyLamaGame/new_flappyLamGame.dart
+++ lama_app/lib/newFlappyLamaGame/new_flappyLamGame.dart
@@ -2,15 +2,23 @@ import 'dart:math';
 import 'dart:ui';
 import 'dart:developer' as developer;
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/input.dart';
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'package:lama_app/app/model/highscore_model.dart';
 import 'package:lama_app/app/repository/user_repository.dart';
+import 'package:flutter/animation.dart';
 
-class Player extends SpriteComponent {
+enum PlayerState { crashed, jumping, running, waiting }
+
+class Player extends SpriteAnimationGroupComponent<PlayerState>
+    with HasGameRef<NewFlappyGame>, CollisionCallbacks {
+  Player() : super(size: Vector2(90, 88));
+
   // Métodos do jogador.
   void gravity(Vector2 target, double time) {
     super.position.moveToTarget(target, 2.5 * time * time);
@@ -29,14 +37,33 @@ class NewFlappyGame extends FlameGame with TapDetector {
   Future<void>? onLoad() async {
     await super.onLoad();
 
-    final Sprite playerSprite =
-        Sprite(await images.load('png/lama_animation.png'));
+    // loads the spritesheet from assets
+    final spriteSheet = SpriteSheet(
+      image: await images.load('png/lama_animation.png'),
+      srcSize: Vector2(16.0, 18.0),
+    );
 
-    player = Player()
+    // idle / hover animation
+    final idleAnimation =
+        spriteSheet.createAnimation(row: 0, from: 0, to: 4, stepTime: 0.1);
+
+    // up animation
+    final upAnimation =
+        spriteSheet.createAnimation(row: 0, from: 5, to: 8, stepTime: 0.1);
+
+    // fall animation
+    final fallAnimation =
+        spriteSheet.createAnimation(row: 0, from: 9, to: 12, stepTime: 0.1);
+
+    /*  final Sprite playerSprite =
+        Sprite(await images.load('png/lama_animation.png')); */
+
+    SpriteAnimation animation = idleAnimation;
+    /*    player = Player()
       ..sprite = playerSprite
       ..size = playerSprite.srcSize * 2
       ..position = size / 2
-      ..anchor = Anchor.center;
+      ..anchor = Anchor.center; */
     add(player);
   }
 
