28d0b41b2f50a31164558a05db659da55ca863b3
nicosoethe <ni.soethe@gmail.com>
2022-06-13 13:28:50 +0200

    added state

--- lama_app/lib/app/bloc/create_taskset_bloc.dart
+++ lama_app/lib/app/bloc/create_taskset_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:lama_app/app/state/create_taskset_state.dart';
 
 import '../event/create_taskset_event.dart';
 import '../state/taskset_creation_state.dart';
@@ -17,20 +18,18 @@ import '../task-system/taskset_model.dart';
 ///
 /// Author: N. Soethe
 /// latest Changes: 01.06.2022
-class CreateTasksetBloc extends Bloc<CreateTasksetEvent, TasksetCreationState> {
+class CreateTasksetBloc extends Bloc<CreateTasksetEvent, CreateTasksetState> {
 
-  Taskset _taskset = Taskset("", "", 1);
-
-  CreateTasksetBloc({TasksetCreationState initialState}) : super(initialState);
+  CreateTasksetBloc({CreateTasksetState initialState}) : super(CreateTasksetState(taskset: Taskset("", "", 1)));
 
   @override
-  Stream<TasksetCreationState> mapEventToState(CreateTasksetEvent event) async* {
-    if(event is CreateTasksetChangeName) _taskset.name = event.name;
-    if(event is CreateTasksetChangeSubject) _taskset.subject = event.subject;
-    if(event is CreateTasksetChangeGrade) _taskset.grade = event.grade;
+  Stream<CreateTasksetState> mapEventToState(CreateTasksetEvent event) async* {
+    if(event is CreateTasksetChangeName) state.taskset.name = event.name;
+    if(event is CreateTasksetChangeSubject) state.taskset.subject= event.subject;
+    if(event is CreateTasksetChangeGrade) state.taskset.grade = event.grade;
     if(event is CreateTasksetAbort) _abort(event.context);
     //TODO: Delete this after implementation
-    print("Name: ${_taskset.name}, Subject: ${_taskset.subject}, Grade: ${_taskset.grade}\n");
+    print("Name: ${state.taskset.name}, Subject: ${state.taskset.subject}, Grade: ${state.taskset.grade}\n");
   }
 
   /// private method to abort the current creation process
--- lama_app/lib/app/state/create_taskset_state.dart
+++ lama_app/lib/app/state/create_taskset_state.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:lama_app/app/bloc/taskset_creation_cart_bloc.dart';
 import 'package:lama_app/app/screens/admin_menu_folder/taskset_creation_cart_screen.dart';
+import 'package:lama_app/app/screens/admin_menu_folder/taskset_manage/bloc/taskset_manage_event.dart';
+
+import '../task-system/taskset_model.dart';
 
 /// States used by [TasksetCreationCartScreen] and [TasksetCreationCartBloc]
 ///
 /// Author: N. Soethe
 /// latest Changes: 01.06.2022
 
-abstract class TasksetCreationListState {}
+class CreateTasksetState {
+  Taskset taskset;
+
+  CreateTasksetState({
+    @required this.taskset,
+  });
+}
