95a5a355b866a595ca8be6804c1843a30dca31c6
nicosoethe <ni.soethe@gmail.com>
2022-06-14 14:49:23 +0200

    Try out for MoneyTask

--- lama_app/lib/app/screens/admin_menu_folder/creation_choose_task_screen.dart
+++ lama_app/lib/app/screens/admin_menu_folder/creation_choose_task_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lama_app/app/bloc/create_taskset_bloc.dart';
 import 'package:lama_app/app/screens/admin_menu_folder/widgets/custom_appbar.dart';
+import 'package:lama_app/app/screens/task_erstellen/money_einstellen_screen.dart';
 import 'package:lama_app/app/state/create_taskset_state.dart';
 import 'package:lama_app/util/LamaColors.dart';
 
@@ -31,14 +32,32 @@ class ChooseTaskScreen extends StatelessWidget {
       builder: (context, state) {
           return Card(
             margin: const EdgeInsets.all(8.0),
-            color: LamaColors.bluePrimary,
-            child: Padding(
+            child: Container(
               padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: LamaColors.bluePrimary
+                  )),
               child: Row(
                 children: [
                   Text(
                     "MoneyTask",
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: LamaColors.bluePrimary),
+                  ),
+                  IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => BlocProvider.value(
+                                        value:
+                                            BlocProvider.of<CreateTasksetBloc>(
+                                                context),
+                                        child: MoneyEinstellenScreen(),
+                                      )));
+                        }, // navigiere zu MoneyTask Erstellung Screen
+                    icon: Icon(Icons.keyboard_arrow_right),
+                    color: LamaColors.bluePrimary,
                   ),
                 ],
               ),
---
+++ lama_app/lib/app/screens/task_erstellen/money_einstellen_screen.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lama_app/app/screens/admin_menu_folder/widgets/custom_appbar.dart';
+
+import '../../../util/LamaColors.dart';
+import '../../../util/LamaTextTheme.dart';
+import '../../bloc/user_selection_bloc.dart';
+import '../user_selection_screen.dart';
+
+class MoneyEinstellenScreen extends StatefulWidget {
+  @override
+  MoneyEinstellenScreenState createState() => MoneyEinstellenScreenState();
+}
+
+class MoneyEinstellenScreenState extends State<MoneyEinstellenScreen> {
+  final List<Item> _data = generateItems(1);
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String von;
+  String bis;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: CustomAppbar(size: screenSize.width / 5,titel: "Tasksets erstellen", color: LamaColors.bluePrimary),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(15),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Geldbetrag",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 15, bottom: 15, right: 30),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(right: 30),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Von',
+                            ),
+                            validator: (text) {
+                              if (text == null || text.isEmpty) {
+                                return "Beitrag fehlt!";
+                              }
+                              return null;
+                            },
+                            onSaved: (text) {
+                              von = text;
+                            },
+                            onChanged: (text) =>
+                                setState(() => this.von = text),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(left: 30),
+                          child: Text(
+                            "bis",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(left: 30),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Bis',
+                            ),
+                            validator: (text) {
+                              if (text == null || text.isEmpty) {
+                                return "Beitrag fehlt!";
+                              }
+                              return null;
+                            },
+                            onSaved: (text) {
+                              von = text;
+                            },
+                            onChanged: (text) =>
+                                setState(() => this.bis = text),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+            Container(
+              margin: EdgeInsets.only(top: 15, bottom: 15),
+              child: ExpansionPanelList.radio(
+                expansionCallback: (int index, bool isExpanded) {},
+                children: _data.map<ExpansionPanelRadio>((Item item) {
+                  return ExpansionPanelRadio(
+                      value: item.id,
+                      headerBuilder: (BuildContext context, bool isExpanded) {
+                        return ListTile(
+                          title: Text(item.headerValue),
+                        );
+                      },
+                      body: CheckboxListTile(
+                        title: Text("Nur volle Euro"),
+                        onChanged: (bool value) {
+                          setState(() {
+                            timeDilation = value ? 10.0 : 1.0;
+                          });
+                        },
+                        value: timeDilation != 1.0,
+                      ));
+                }).toList(),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Erreichbare Lamacoins',
+                ),
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return "Beitrag fehlt!";
+                  }
+                  return null;
+                },
+                onSaved: (text) {
+                  von = text;
+                },
+                onChanged: (text) => setState(() => this.von = text),
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.all(50),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary: Colors.white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MoneyEinstellenScreen()),
+                        );
+                        /*   if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Processing Data')),
+                                );
+                              }
+                              _formKey.currentState?.save();
+                              print(tasksetName);
+                              print(kurzBeschreibung);
+                              print(value);
+                              print(value2); */
+                      },
+                      child: Text.rich(
+                        TextSpan(
+                            text: 'Preview',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black)),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MoneyEinstellenScreen()),
+                        );
+                        /*   if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Processing Data')),
+                                );
+                              }
+                              _formKey.currentState?.save();
+                              print(tasksetName);
+                              print(kurzBeschreibung);
+                              print(value);
+                              print(value2); */
+                      },
+                      child: Text.rich(
+                        TextSpan(
+                            text: 'Hinzufügen',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                    )
+                  ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Item {
+  Item({
+    @required this.id,
+    @required this.expandedValue,
+    @required this.headerValue,
+  });
+
+  int id;
+  String expandedValue;
+  String headerValue;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List<Item>.generate(numberOfItems, (int index) {
+    return Item(
+      id: index,
+      headerValue: 'Erweiterte Optionen',
+      expandedValue: "Nur volle Euro",
+    );
+  });
+}
